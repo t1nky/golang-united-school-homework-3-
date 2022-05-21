b816e347ab834967052f7c9891998ab82ce9e735
Alex <haiiryy@gmail.com>
2022-05-21 13:46:27 +0400

    Fixed task3

--- task03-map.go
+++ task03-map.go
@@ -3,11 +3,17 @@ package homework
 import "sort"
 
 func sortMapValues(input map[int]string) (result []string) {
-	var a []string
+	var a []int
+	var out []string
 	//Place your code here
-	for _, v := range input {
-		a = append(a, v)
+	for k := range input {
+		a = append(a, k)
 	}
-	sort.Sort(sort.StringSlice(a))
-	return
+	sort.Ints(a)
+
+	for v := range a {
+		out = append(out, input[v])
+	}
+
+	return out
 }
