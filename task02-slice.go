d59976b6a8ea08658807a6f9b6601a6c6e5f2f69
Alex <haiiryy@gmail.com>
2022-05-21 13:52:20 +0400

    Fixed reverse

--- task02-slice.go
+++ task02-slice.go
@@ -2,12 +2,12 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	temp := int64(0);
+	temp := int64(0)
 	length := len(input)
-	for i := 0; i < int(length / 2); i++ {
+	for i := 0; i < int(length/2); i++ {
 		temp = input[i]
-		input[i] = input[length - i]
-		input[length - i] = temp
+		input[i] = input[length-i-1]
+		input[length-i-1] = temp
 	}
 
 	return input
